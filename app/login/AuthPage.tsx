26d83034f819dd39d78261ccf91bfca0d68d3519
bonison01 <164063307+bonison01@users.noreply.github.com>
2025-09-10 15:32:34 +0530

    sdf

--- app/login/AuthPage.tsx
+++ app/login/AuthPage.tsx
@@ -24,7 +24,7 @@ export default function AuthPage() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.svg"
+          src="/logo.png"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
